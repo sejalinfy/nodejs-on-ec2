cfdd5fc6e1d5386bff6adee61b2cb7274d0b1b70
sejalinfy <147516958+sejalinfy@users.noreply.github.com>
2024-07-15 17:34:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
 var http = require('http');
-
 http.createServer(function (req, res) {
-  // Set the response HTTP header with HTTP status and Content type
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  
   // Route the request based on the URL
   switch(req.url) {
     case '/':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end('Welcome to A Monk in Cloud');
       break;
     case '/about':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end('About A Monk in Cloud');
       break;
     case '/contact':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end('Contact A Monk in Cloud');
       break;
     default:
-      res.writeHead(404);
+      res.writeHead(404, {'Content-Type': 'text/plain'});
       res.end('404 Not Found');
   }
-}).listen(80);
+}).listen(3000);
 
-console.log('Server running on port 80');
+console.log('Server running on port 3000');
